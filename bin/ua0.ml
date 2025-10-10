8f7149427ddf6e7255c890116f5e3de1a37e6f52
Pierre-Évariste Dagand <doc@evr.ist>
2025-10-10 10:15:06 +0200

    Repair binary for ua0

--- bin/ua0.ml
+++ bin/ua0.ml
@@ -1,6 +1,80 @@
 let () = Printexc.print_backtrace stderr
 let () = Printexc.record_backtrace true
 
+module StringUtil = struct
+  let rec chunks current acc n s =
+    match s with
+    | [] -> acc
+    | t :: q -> (
+        let s = Printf.sprintf "%s%c" current t in
+        match String.length s with
+        | l when l = n -> chunks String.empty (s :: acc) n q
+        | _ -> chunks s acc n q)
+
+  (** [chunks n s] sépare [s] en une liste de [n] strings *)
+  let chunks ?(rev = false) n s =
+    let s = chunks String.empty [] n (List.of_seq @@ String.to_seq s) in
+    if rev then s else List.rev s
+end
+
+module Bits = struct
+  let of_int64 ?pad n =
+    let ( mod ) = Int64.unsigned_rem in
+    let ( / ) = Int64.unsigned_div in
+    let rec aux n list =
+      let quotient = n / 2L in
+      let res = n mod 2L in
+      if quotient = 0L then res :: list else aux quotient (res :: list)
+    in
+    let list = aux n [] in
+    let length = List.length list in
+    match pad with
+    | None -> list
+    | Some padding ->
+        let leading = max 0 (padding - length) in
+        List.init leading (fun _ -> 0L) @ list
+
+  let of_int ?pad n = List.map (Int64.equal 1L) @@ of_int64 ?pad n
+end
+
+module Io = struct
+  let read_all_chan ch = really_input_string ch (in_channel_length ch)
+  let read_all string = In_channel.with_open_bin string read_all_chan
+end
+
+module Common = struct
+  let from_hex_string s =
+    s |> String.to_seq
+    |> Seq.filter (Fun.negate @@ ( = ) ' ')
+    |> String.of_seq |> StringUtil.chunks 2
+    |> List.map (fun s ->
+           let s = "0x" ^ s in
+           Bits.of_int ~pad:8 @@ Int64.of_string s)
+    |> List.flatten
+
+  let file_to_bools file =
+    let content = Io.read_all file in
+    from_hex_string content
+end
+
+module ListUtil = struct
+  let rec chunks current acc n s =
+    match s with
+    | [] -> current :: acc
+    | t :: q -> (
+        match List.length current with
+        | l when l = n -> chunks [ t ] (current :: acc) n q
+        | _ ->
+            let current = current @ [ t ] in
+            chunks current acc n q)
+
+  (** [chunks n s] sépare [s] en une liste de chaque liste ou chaque sous liste
+      est de longeur [n]. *)
+  let chunks ?(rev = false) n l =
+    let s = chunks [] [] n l in
+    if rev then s else List.rev s
+end
+
 module RowsCols = struct
   let tabulate f =
     Ua0.Value.tabulate 4 @@ fun c ->
@@ -13,8 +87,7 @@ end
 
 module Gift = struct
   let round_constants =
-    List.map
-      (Ua0.Ex.Bits.of_int ~pad:16)
+    List.map (Bits.of_int ~pad:16)
       [
         0x80_01L;
         0x80_03L;
@@ -192,7 +265,7 @@ module Gift = struct
     pp Format.err_formatter chars
 
   let uvconsts key =
-    let key = Ua0.Ex.ListUtil.chunks 16 key in
+    let key = ListUtil.chunks 16 key in
     let us, vs = uvs key in
     let bottom = List.map (Ua0.Value.map' (Fun.const false)) us in
     let consts =
@@ -249,8 +322,8 @@ let eval ~double ast symbole keys texts =
   let kps =
     Seq.map2
       (fun k t ->
-        let k = Ua0.Ex.Common.file_to_bools k in
-        let t = Ua0.Ex.Common.file_to_bools t in
+        let k = Common.file_to_bools k in
+        let t = Common.file_to_bools t in
         (k, t))
       (Queue.to_seq keys) (Queue.to_seq texts)
   in

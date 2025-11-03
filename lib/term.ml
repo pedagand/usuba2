7a81d0f690d39bd86c0b5b28e66a06112b6b18e9
Pierre-Evariste Dagand <doc@evr.ist>
2025-11-03 13:44:00 +0100

    term: smart constructors in definition module

--- lib/term.ml
+++ lib/term.ml
@@ -112,3 +112,58 @@ let pp =
 let pp_sterm =
   pp_sterm_ Ident.TermIdent.pp Ident.TyIdent.pp Ident.TyDeclIdent.pp
     Ident.FnIdent.pp
+
+module S = struct
+  let s sterm = Synth sterm
+  let ann tm ty = Ann (tm, ty)
+  let true' = True
+  let false' = False
+  let v variable = Var variable
+  let vfn fn_ident = Fn { fn_ident }
+
+  let log message variables k =
+    let k = k () in
+    Log { message; variables; k }
+
+  let log_ message variables k = Log { message; variables; k }
+  let lookup lterm index = Lookup { lterm; index }
+  let ( .%() ) = lookup
+  let let_ variable term k = Let { variable; term; k }
+
+  let let' variable term k =
+    let variable = Ident.TermIdent.fresh variable in
+    Let { variable; term; k = k (v variable) }
+
+  let fn_call ?resolve fn_name args =
+    FnCall { fn_name = Left fn_name; ty_resolve = resolve; args }
+
+  let v_call ?resolve variable_name args =
+    FnCall { fn_name = Right variable_name; ty_resolve = resolve; args }
+
+  let circ t = Circ t
+  let lift tys func = Lift { tys; func }
+
+  let reindex lhs rhs lterm =
+    assert (lhs <> []);
+    assert (rhs <> []);
+    Reindex { lhs; rhs; lterm }
+
+  let ( lxor ) lhs rhs = Operator (Xor (lhs, rhs))
+  let ( land ) lhs rhs = Operator (And (lhs, rhs))
+  let ( lor ) lhs rhs = Operator (Or (lhs, rhs))
+  let lnot term = Operator (Not term)
+  let cstr ty terms = Constructor { ty; terms }
+
+  let let_plus variable lterm ands k =
+    let variable = Ident.TermIdent.fresh variable in
+    let ands =
+      List.map
+        (fun (v, term) ->
+          let v = Ident.TermIdent.fresh v in
+          (v, term))
+        ands
+    in
+    let vand = List.map fst ands in
+    let term = k variable vand in
+    LetPlus { variable; lterm; ands; term }
+end
--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -83,171 +83,170 @@ let fail_typecheck ctx tm ty =
     (fun _ -> ignore (Ua0.Typecheck.typecheck (ctx_of ctx) ty tm))
 
 let () =
-  let open Scstr in
   run "typesynth"
     [
       ( "Var",
         [
           test_case "in-Bool" `Quick (fun () ->
-              check_typesynth Env0 Term.(v x) Ty.S.bool);
+              check_typesynth Env0 Term.S.(v x) Ty.S.bool);
           test_case "in-Var" `Quick (fun () ->
-              check_typesynth Env0 Term.(v y) Ty.S.(v alpha));
+              check_typesynth Env0 Term.S.(v y) Ty.S.(v alpha));
           test_case "in-App" `Quick (fun () ->
-              check_typesynth Env0 Term.(v z) Ty.S.(apps [ _F; _G ] bool));
+              check_typesynth Env0 Term.S.(v z) Ty.S.(apps [ _F; _G ] bool));
         ] );
       ( "Fn",
         [
           test_case "in" `Quick (fun () ->
               check_typesynth Env0
-                Term.(vfn f)
+                Term.S.(vfn f)
                 Ty.S.(fn ~tyvars:(Some alpha) ty_f.parameters ty_f.return_type));
         ] );
       ( "Lookup",
         [
           test_case "in range" `Quick (fun () ->
-              check_typesynth Env0 Term.((v z).%(2)) Ty.S.(_G @ bool));
+              check_typesynth Env0 Term.S.((v z).%(2)) Ty.S.(_G @ bool));
           test_case "out of range" `Quick (fun () ->
-              fail_typesynth Env0 Term.((v z).%(4)));
+              fail_typesynth Env0 Term.S.((v z).%(4)));
           test_case "not Naperian" `Quick (fun () ->
-              fail_typesynth Env0 Term.((v x).%(2)));
+              fail_typesynth Env0 Term.S.((v x).%(2)));
         ] );
       ( "Operator",
         [
           test_case "xor" `Quick (fun () ->
-              check_typesynth Env0 Term.(s (v x) lxor s (v x)) Ty.S.bool);
+              check_typesynth Env0 Term.S.(s (v x) lxor s (v x)) Ty.S.bool);
           test_case "no ad-hoc polymorphism" `Quick (fun () ->
-              fail_typesynth Env0 Term.(s (v x) lxor s (v y)));
+              fail_typesynth Env0 Term.S.(s (v x) lxor s (v y)));
         ] );
       ( "FnCall",
         [
           test_case "well-typed" `Quick (fun () ->
               check_typesynth Env0
-                Term.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v x); s (v y) ])
+                Term.S.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v x); s (v y) ])
                 Ty.S.(v alpha));
           test_case "well-typed instanciated" `Quick (fun () ->
               check_typesynth Env0
-                Term.(fn_call ~resolve:Ty.S.bool f [ s (v x); s (v x) ])
+                Term.S.(fn_call ~resolve:Ty.S.bool f [ s (v x); s (v x) ])
                 Ty.S.bool);
           test_case "no arguments" `Quick (fun () ->
-              fail_typesynth Env0 Term.(fn_call ~resolve:Ty.S.(v alpha) f []));
+              fail_typesynth Env0 Term.S.(fn_call ~resolve:Ty.S.(v alpha) f []));
           test_case "insufficiently applied" `Quick (fun () ->
               fail_typesynth Env0
-                Term.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v x) ]));
+                Term.S.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v x) ]));
           test_case "wrong args" `Quick (fun () ->
               fail_typesynth Env0
-                Term.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v y); s (v x) ]));
+                Term.S.(fn_call ~resolve:Ty.S.(v alpha) f [ s (v y); s (v x) ]));
           test_case "ill-typed instanciation" `Quick (fun () ->
               fail_typesynth Env0
-                Term.(fn_call ~resolve:Ty.S.bool f [ s (v x); s (v y) ]));
+                Term.S.(fn_call ~resolve:Ty.S.bool f [ s (v x); s (v y) ]));
         ] );
       ( "Ann",
         [
           test_case "well-typed" `Quick (fun () ->
-              check_typesynth Env0 Term.(ann true' Ty.S.bool) Ty.S.bool);
+              check_typesynth Env0 Term.S.(ann true' Ty.S.bool) Ty.S.bool);
           test_case "ill-typed" `Quick (fun () ->
-              fail_typesynth Env0 Term.(ann (s (vfn f)) Ty.S.bool));
+              fail_typesynth Env0 Term.S.(ann (s (vfn f)) Ty.S.bool));
         ] );
       ( "Circ",
         [
           test_case "well-typed" `Quick (fun () ->
               check_typesynth Env0
-                Term.(circ (v z))
+                Term.S.(circ (v z))
                 Ty.S.(apps [ _F; _F; _G ] bool));
           test_case "not applicative" `Quick (fun () ->
-              fail_typesynth Env0 Term.(circ (v x)));
+              fail_typesynth Env0 Term.S.(circ (v x)));
         ] );
       ( "Reindex",
         [
           test_case "reindex F / G over bool" `Quick (fun () ->
               check_typesynth Env0
-                Term.(reindex [ _F ] [ _G ] (v z))
+                Term.S.(reindex [ _F ] [ _G ] (v z))
                 Ty.S.(apps [ _G; _F ] bool));
           test_case "reindex F / G over H alpha" `Quick (fun () ->
               check_typesynth Env0
-                Term.(reindex [ _F ] [ _G ] (v u))
+                Term.S.(reindex [ _F ] [ _G ] (v u))
                 Ty.S.(apps [ _G; _F; _H ] (v alpha)));
           test_case "reindex F.G / H over alpha" `Quick (fun () ->
               check_typesynth Env0
-                Term.(reindex [ _F; _G ] [ _H ] (v u))
+                Term.S.(reindex [ _F; _G ] [ _H ] (v u))
                 Ty.S.(apps [ _H; _F; _G ] (v alpha)));
           test_case "reindex F / G.H over alpha" `Quick (fun () ->
               check_typesynth Env0
-                Term.(reindex [ _F ] [ _G; _H ] (v u))
+                Term.S.(reindex [ _F ] [ _G; _H ] (v u))
                 Ty.S.(apps [ _G; _H; _F ] (v alpha)));
           test_case "not applicative" `Quick (fun () ->
-              fail_typesynth Env0 Term.(reindex [ _F ] [ _G ] (v x)));
+              fail_typesynth Env0 Term.S.(reindex [ _F ] [ _G ] (v x)));
           test_case "lhs mismatch" `Quick (fun () ->
-              fail_typesynth Env0 Term.(reindex [ _G ] [ _G ] (v z)));
+              fail_typesynth Env0 Term.S.(reindex [ _G ] [ _G ] (v z)));
           test_case "rhs mismatch" `Quick (fun () ->
-              fail_typesynth Env0 Term.(reindex [ _F ] [ _F ] (v z)));
+              fail_typesynth Env0 Term.S.(reindex [ _F ] [ _F ] (v z)));
         ] );
       ( "Lift",
         [
           test_case "well-typed" `Quick (fun () ->
               check_typesynth Env0
-                Term.(lift [ _F; _G ] (vfn f))
+                Term.S.(lift [ _F; _G ] (vfn f))
                 Ty.S.(
                   fn ~tyvars:(Some alpha)
                     [ _F @ _G @ bool; _F @ _G @ v alpha ]
                     (_F @ _G @ v alpha)));
           test_case "not applicative" `Quick (fun () ->
-              fail_typesynth Env0 Term.(circ (v x)));
+              fail_typesynth Env0 Term.S.(circ (v x)));
         ] );
       (*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*)
       ( "True/False",
         [
           test_case "true well-typed" `Quick (fun () ->
-              check_typecheck Env0 Term.true' Ty.S.bool);
+              check_typecheck Env0 Term.S.true' Ty.S.bool);
           test_case "false well-typed" `Quick (fun () ->
-              check_typecheck Env0 Term.false' Ty.S.bool);
+              check_typecheck Env0 Term.S.false' Ty.S.bool);
           test_case "ill-typed" `Quick (fun () ->
-              fail_typecheck Env0 Term.true' Ty.S.(v alpha));
+              fail_typecheck Env0 Term.S.true' Ty.S.(v alpha));
         ] );
       ( "Constructor",
         [
           test_case "well-typed" `Quick (fun () ->
               check_typecheck Env0
-                Term.(cstr _F [ true'; false'; true'; true' ])
+                Term.S.(cstr _F [ true'; false'; true'; true' ])
                 Ty.S.(_F @ bool));
           test_case "ill-typed" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(cstr _F [ true'; false'; true'; true' ])
+                Term.S.(cstr _F [ true'; false'; true'; true' ])
                 Ty.S.(v alpha));
           test_case "arity checked" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(cstr _F [ false'; true'; true' ])
+                Term.S.(cstr _F [ false'; true'; true' ])
                 Ty.S.(_F @ bool));
         ] );
       ( "Let",
         [
           test_case "well-typed" `Quick (fun () ->
               check_typecheck Env0
-                Term.(
+                Term.S.(
                   let' "a" (s (v x) lxor s (v x)) (fun a -> s (s a land s a)))
                 Ty.S.bool);
           test_case "ill-typed argument" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(
+                Term.S.(
                   let' "a" (s (v x) lxor s (v y)) (fun a -> s (s a land s a)))
                 Ty.S.bool);
           test_case "ill-typed application" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(let' "a" (v y) (fun a -> s (s a land s a)))
+                Term.S.(let' "a" (v y) (fun a -> s (s a land s a)))
                 Ty.S.bool);
           test_case "rejecting wrong type" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(let' "a" (v y) (fun _a -> s (v x)))
+                Term.S.(let' "a" (v y) (fun _a -> s (v x)))
                 Ty.S.(v alpha));
         ] );
       ( "Let+",
         [
           test_case "well-typed functor" `Quick (fun () ->
               check_typecheck Env0
-                Term.(let_plus "a" (v z) [] (fun _a _ -> true'))
+                Term.S.(let_plus "a" (v z) [] (fun _a _ -> true'))
                 Ty.S.(_F @ bool));
           test_case "well-typed monoidal functor" `Quick (fun () ->
               check_typecheck Env0
-                Term.(
+                Term.S.(
                   let_plus "a" (v z)
                     [ ("b", v u); ("c", v z) ]
                     (fun _a bs ->
@@ -255,7 +254,7 @@ let () =
                 Ty.S.(apps [ _F; _G ] bool));
           test_case "well-typed monoidal functor" `Quick (fun () ->
               check_typecheck Env0
-                Term.(
+                Term.S.(
                   let_plus "a" (v z)
                     [ ("b", v u); ("c", v z) ]
                     (fun _a bs ->
@@ -263,16 +262,16 @@ let () =
                 Ty.S.(apps [ _F; _G; _H ] (v alpha)));
           test_case "incompatible arg / product monoidal" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(let_plus "a" (v z) [ ("b", v w) ] (fun _a _ -> true'))
+                Term.S.(let_plus "a" (v z) [ ("b", v w) ] (fun _a _ -> true'))
                 Ty.S.(_F @ bool));
           test_case "incompatible arg / product monoidal (dual)" `Quick
             (fun () ->
               fail_typecheck Env0
-                Term.(let_plus "a" (v z) [ ("b", v w) ] (fun _a _ -> true'))
+                Term.S.(let_plus "a" (v z) [ ("b", v w) ] (fun _a _ -> true'))
                 Ty.S.(_G @ bool));
           test_case "incompatible product / product monoidal" `Quick (fun () ->
               fail_typecheck Env0
-                Term.(
+                Term.S.(
                   let_plus "a" (v z)
                     [ ("b", v z); ("c", v w) ]
                     (fun _a _ -> true'))

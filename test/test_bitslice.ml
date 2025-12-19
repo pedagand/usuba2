f1b9f9bc4626853dcb4331d6fe2fa5940d4735c7
Pierre-Evariste Dagand <doc@evr.ist>
2025-12-19 10:58:37 +0100

    Refactor src/ definitions and file loading mechanism

--- test/test_bitslice.ml
+++ test/test_bitslice.ml
@@ -1,121 +1,83 @@
 module Value = Ua0.Value
 module IEnv = Ua0.Pass.Idents.Env
 module EEnv = Ua0.Eval.Env
+open Gen
 
-(* Load & eval `test_reindex2.ua` *)
+(* Load & eval `gift_spec.ua` and `gift_bitslice.ua` *)
 
-let filename = "src/test_reindex2.ua"
+module GiftSpec = struct
+  let symbol = File.load "src/gift_spec.ua"
+  let gift32 = symbol "gift32"
+  let round32 = symbol "round32"
+  let subcells32 = symbol "subcells32"
+end
 
-let env, ast =
-  let file = filename in
-  let ast =
-    In_channel.with_open_bin file (fun ic ->
-        let lexbuf = Lexing.from_channel ic in
-        let () = Lexing.set_filename lexbuf file in
-        Ua0.Parser.module_ Ua0.Lexer.token lexbuf)
-  in
-  Ua0.Pass.Idents.of_string_ast_env ast
-
-let prog = Ua0.Eval.eval ast
-
-let symbol ienv functions s =
-  let f = IEnv.find_fn_ident s ienv in
-  EEnv.Functions.find f functions
-
-(* Generators *)
-
-let value2 (a, b) = Value.VArray [| a; b |]
-let value4 (a, b, c, d) = Value.VArray [| a; b; c; d |]
-let qvalue_bool = QCheck.(bool |> map (fun v -> Value.VBool v))
-
-let qvalue4 =
-  QCheck.(tup4 qvalue_bool qvalue_bool qvalue_bool qvalue_bool |> map value4)
-
-let qvalue_4x4 = QCheck.(tup4 qvalue4 qvalue4 qvalue4 qvalue4 |> map value4)
-
-let qvalue_4x4x4 =
-  QCheck.(tup4 qvalue_4x4 qvalue_4x4 qvalue_4x4 qvalue_4x4 |> map value4)
-
-let qvalue_2 = QCheck.(tup2 qvalue_bool qvalue_bool |> map value2)
-let qvalue_4x2 = QCheck.(tup4 qvalue_2 qvalue_2 qvalue_2 qvalue_2 |> map value4)
-
-let qvalue_4x4x2 =
-  QCheck.(tup4 qvalue_4x2 qvalue_4x2 qvalue_4x2 qvalue_4x2 |> map value4)
-
-let qvalue_4x4x4x2 =
-  QCheck.(
-    tup4 qvalue_4x4x2 qvalue_4x4x2 qvalue_4x4x2 qvalue_4x4x2 |> map value4)
+module GiftBitslice = struct
+  let symbol = File.load "src/gift_bitslice.ua"
+  let transpose32 = symbol "transpose32"
+  let transpose32_inv = symbol "transpose32_inv"
+  let gift32 = symbol "gift32_bitslice"
+  let round32 = symbol "round32_bitslice"
+  let subcells32 = symbol "subcells32_bitslice"
+end
 
 (* QCheck tests *)
 
-let test_transpose32 ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
-  let transpose32_inv = symbol ienv functions "transpose32_inv" in
+let test_transpose32 =
   QCheck.Test.make ~name:"transpose32 . transpose32_inv = id" qvalue_4x4x4
   @@ fun value ->
   Value.equal value
-    ([ value ] |> transpose32 |> (fun x -> [ x ]) |> transpose32_inv)
+    ([ value ] |> GiftBitslice.transpose32
+    |> (fun x -> [ x ])
+    |> GiftBitslice.transpose32_inv)
 
-let test_transpose32_id ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
+let test_transpose32_id =
   QCheck.Test.make ~name:"transpose32 != id" qvalue_4x4x4 @@ fun value ->
-  not (Value.equal value ([ value ] |> transpose32))
+  not (Value.equal value ([ value ] |> GiftBitslice.transpose32))
 
-let test_transpose32_inv ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
-  let transpose32_inv = symbol ienv functions "transpose32_inv" in
+let test_transpose32_inv =
   QCheck.Test.make ~name:"transpose32_inv . transpose32 = id" qvalue_4x4x4
   @@ fun value ->
   Value.equal value
-    ([ value ] |> transpose32_inv |> (fun x -> [ x ]) |> transpose32)
+    ([ value ] |> GiftBitslice.transpose32_inv
+    |> (fun x -> [ x ])
+    |> GiftBitslice.transpose32)
 
-let test_gift32 ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
-  let transpose32_inv = symbol ienv functions "transpose32_inv" in
-  let gift32 = symbol ienv functions "gift32" in
-  let gift32_bitslice = symbol ienv functions "gift32_bitslice" in
+let test_gift32 =
   QCheck.Test.make
     ~name:"gift32_bitslice = transpose32 . gift32 . transpose32_inv"
     (QCheck.pair qvalue_4x4x4x2
        (QCheck.array_of_size (QCheck.Gen.return 28) qvalue_4x4x4x2))
   @@ fun (state, keys) ->
   Value.equal
-    (gift32_bitslice [ state; Value.VArray keys ])
-    (let state = transpose32 [ state ] in
-     let keys = keys |> Array.map (fun x -> transpose32 [ x ]) in
-     let s = gift32 [ state; Value.VArray keys ] in
-     transpose32_inv [ s ])
+    (GiftBitslice.gift32 [ state; Value.VArray keys ])
+    (let state = GiftBitslice.transpose32 [ state ] in
+     let keys = keys |> Array.map (fun x -> GiftBitslice.transpose32 [ x ]) in
+     let s = GiftSpec.gift32 [ state; Value.VArray keys ] in
+     GiftBitslice.transpose32_inv [ s ])
 
-let test_round32 ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
-  let transpose32_inv = symbol ienv functions "transpose32_inv" in
-  let round32 = symbol ienv functions "round32" in
-  let round32_bitslice = symbol ienv functions "round32_bitslice" in
+let test_round32 =
   QCheck.Test.make
     ~name:"round32_bitslice = transpose32 . rounde32 . transpose32_inv"
     (QCheck.pair qvalue_4x4x4x2 qvalue_4x4x4x2)
   @@ fun (state, key) ->
   Value.equal
-    (round32_bitslice [ state; key ])
-    (let state = transpose32 [ state ] in
-     let key = transpose32 [ key ] in
-     let s = round32 [ state; key ] in
-     transpose32_inv [ s ])
+    (GiftBitslice.round32 [ state; key ])
+    (let state = GiftBitslice.transpose32 [ state ] in
+     let key = GiftBitslice.transpose32 [ key ] in
+     let s = GiftSpec.round32 [ state; key ] in
+     GiftBitslice.transpose32_inv [ s ])
 
-let test_subcells32 ienv functions =
-  let transpose32 = symbol ienv functions "transpose32" in
-  let transpose32_inv = symbol ienv functions "transpose32_inv" in
-  let subcells32 = symbol ienv functions "subcells32" in
-  let subcells32_bitslice = symbol ienv functions "subcells32_bitslice" in
+let test_subcells32 =
   QCheck.Test.make
     ~name:"subcells32_bitslice = transpose32 . subcells32 . transpose32_inv"
     qvalue_4x4x4x2
   @@ fun state ->
   Value.equal
-    (subcells32_bitslice [ state ])
-    (let state = transpose32 [ state ] in
-     let state = subcells32 [ state ] in
-     transpose32_inv [ state ])
+    (GiftBitslice.subcells32 [ state ])
+    (let state = GiftBitslice.transpose32 [ state ] in
+     let state = GiftSpec.subcells32 [ state ] in
+     GiftBitslice.transpose32_inv [ state ])
 
 let () =
   let open Alcotest in
@@ -123,13 +85,8 @@ let () =
     [
       ( "transpose32",
         List.map QCheck_alcotest.to_alcotest
-          [
-            test_transpose32_id env prog;
-            test_transpose32 env prog;
-            test_transpose32_inv env prog;
-          ] );
-      ( "gift32",
-        [ QCheck_alcotest.to_alcotest ~long:true (test_gift32 env prog) ] );
-      ("round32", [ QCheck_alcotest.to_alcotest (test_round32 env prog) ]);
-      ("subcells32", [ QCheck_alcotest.to_alcotest (test_subcells32 env prog) ]);
+          [ test_transpose32_id; test_transpose32; test_transpose32_inv ] );
+      ("gift32", [ QCheck_alcotest.to_alcotest ~long:true test_gift32 ]);
+      ("round32", [ QCheck_alcotest.to_alcotest test_round32 ]);
+      ("subcells32", [ QCheck_alcotest.to_alcotest test_subcells32 ]);
     ]
--- test/test_fixslice.ml
+++ test/test_fixslice.ml
@@ -1,30 +1,26 @@
 module Value = Ua0.Value
-module IEnv = Ua0.Pass.Idents.Env
-module EEnv = Ua0.Eval.Env
 
-let filename = "src/test_reindex_fs.ua"
+(* Load & eval `gift_fixslice.ua` *)
 
-let env, ast =
-  let file = filename in
-  let ast =
-    In_channel.with_open_bin file (fun ic ->
-        let lexbuf = Lexing.from_channel ic in
-        let () = Lexing.set_filename lexbuf file in
-        Ua0.Parser.module_ Ua0.Lexer.token lexbuf)
-  in
-  Ua0.Pass.Idents.of_string_ast_env ast
+module GiftBitslice = struct
+  let symbol = File.load "src/gift_bitslice.ua"
+  let permbits = symbol "permbits_bitslice"
+end
 
-let prog = Ua0.Eval.eval ast
-let qvalue_bool = QCheck.(bool |> map (fun v -> Value.VBool v))
-let value4 (a, b, c, d) = Value.VArray [| a; b; c; d |]
-
-let qvalue4 =
-  QCheck.(tup4 qvalue_bool qvalue_bool qvalue_bool qvalue_bool |> map value4)
-
-let qvalue_4x4 = QCheck.(tup4 qvalue4 qvalue4 qvalue4 qvalue4 |> map value4)
-
-let qvalue_4x4x4 =
-  QCheck.(tup4 qvalue_4x4 qvalue_4x4 qvalue_4x4 qvalue_4x4 |> map value4)
+module GiftFixslice = struct
+  let symbol = File.load "src/gift_fixslice.ua"
+  let permbits_mod_0_ = symbol "permbits_fixslice_mod_0_"
+  let permbits_mod_0_4 = symbol "permbits_fixslice_mod_0_4"
+  let rev_rotate_1 = symbol "rev_rotate_1"
+  let inv_rev_rotate_1 = symbol "inv_rev_rotate_1"
+  let transpose = symbol "transpose"
+  let inv_transpose = symbol "inv_transpose"
+  let permbits_mod0 = symbol "permbits_fixslice_mod_0_4"
+  let permbits_mod1 = symbol "permbits_fixslice_mod_1_"
+  let permbits_mod2 = symbol "permbits_fixslice_mod_2_"
+  let permbits_mod3 = symbol "permbits_fixslice_mod_3_"
+  let map_rev_rotate_1 = symbol "map_rev_rotate_1"
+end
 
 let test_compose_id ~eq ~name f1 f2 ty =
   QCheck.Test.make ~name ty @@ fun value ->
@@ -37,10 +33,6 @@ let apply2 f = fun args -> f [ f args ]*)
 let compose g f args = g [ f args ]
 let ( $ ) = compose
 
-let symbol ienv functions s =
-  let f = IEnv.find_fn_ident s ienv in
-  EEnv.Functions.find f functions
-
 let test_eq_f ?verbose ~eq ~name lhs rhs ty =
   QCheck.Test.make ~name ty @@ fun value ->
   let lhs = lhs [ value ] in
@@ -52,108 +44,84 @@ let test_eq_f ?verbose ~eq ~name lhs rhs ty =
   in
   eq lhs rhs
 
-let test_permibits_fixslice_transformation ienv functions =
-  let permbits_fixslice_mod_0_ =
-    symbol ienv functions "permbits_fixslice_mod_0_"
-  in
-  let permbits_fixslice_mod_0_4 =
-    symbol ienv functions "permbits_fixslice_mod_0_4"
-  in
-  QCheck.Test.make ~name:"Permbits fixslice transformation" qvalue_4x4x4
+let test_permibits_fixslice_transformation =
+  QCheck.Test.make ~name:"Permbits fixslice transformation" Gen.qvalue_4x4x4
   @@ fun value ->
   let args = [ value ] in
-  Value.equal (permbits_fixslice_mod_0_ args) (permbits_fixslice_mod_0_4 args)
+  Value.equal
+    (GiftFixslice.permbits_mod_0_ args)
+    (GiftFixslice.permbits_mod_0_4 args)
 
-let test_sigma_compose ienv functions =
-  let rev_rotate_1 = symbol ienv functions "rev_rotate_1" in
-  let inv_rev_rotate_1 = symbol ienv functions "inv_rev_rotate_1" in
+let test_sigma_compose =
   test_compose_id ~eq:Value.equal ~name:"Sigma | InvSigma compose id"
-    rev_rotate_1 inv_rev_rotate_1 qvalue_4x4
+    GiftFixslice.rev_rotate_1 GiftFixslice.inv_rev_rotate_1 Gen.qvalue_4x4
 
-let test_sigma4_id ienv functions =
-  let sigma = symbol ienv functions "rev_rotate_1" in
-  QCheck.Test.make ~name:"Sigma compose 4 = id" qvalue_4x4 @@ fun value ->
+let test_sigma4_id =
+  let sigma = GiftFixslice.rev_rotate_1 in
+  QCheck.Test.make ~name:"Sigma compose 4 = id" Gen.qvalue_4x4 @@ fun value ->
   Value.equal value ((sigma $ sigma $ sigma $ sigma) [ value ])
 
-let test_compose_transposition ienv functions =
-  let transposition = symbol ienv functions "transpose" in
-  let inv_transposition = symbol ienv functions "inv_transpose" in
+let test_compose_transposition =
   test_compose_id ~eq:Value.equal ~name:"tranposition | inv - compose"
-    transposition inv_transposition qvalue_4x4
+    GiftFixslice.transpose GiftFixslice.inv_transpose Gen.qvalue_4x4
 
-let test_permbits_fixslice_mod_0 ienv functions =
-  let pb = symbol ienv functions "permbits_bitslice" in
-  let pf_mod0 = symbol ienv functions "permbits_fixslice_mod_0_4" in
-  let sync = symbol ienv functions "map_rev_rotate_1" in
-  test_eq_f ~eq:Value.equal ~name:"Permbits fixslice : mod 0" pb
-    (sync $ pf_mod0) qvalue_4x4x4
+let test_permbits_fixslice_mod_0 =
+  test_eq_f ~eq:Value.equal ~name:"Permbits fixslice : mod 0"
+    GiftBitslice.permbits
+    (GiftFixslice.map_rev_rotate_1 $ GiftFixslice.permbits_mod0)
+    Gen.qvalue_4x4x4
 
-let test_permbits_fixslice_mod_1 ienv functions =
-  let pb = symbol ienv functions "permbits_bitslice" in
-  let pf_mod0 = symbol ienv functions "permbits_fixslice_mod_0_4" in
-  let pf_mod1 = symbol ienv functions "permbits_fixslice_mod_1_" in
-  let map_rev_rotate_1 = symbol ienv functions "map_rev_rotate_1" in
+let test_permbits_fixslice_mod_1 =
   test_eq_f ~verbose:Value.pp ~eq:Value.equal ~name:"Permbits fixslice : mod 1"
-    (pb $ pb)
-    (map_rev_rotate_1 $ map_rev_rotate_1 $ pf_mod1 $ pf_mod0)
-    qvalue_4x4x4
+    (GiftBitslice.permbits $ GiftBitslice.permbits)
+    (GiftFixslice.map_rev_rotate_1 $ GiftFixslice.map_rev_rotate_1
+   $ GiftFixslice.permbits_mod1 $ GiftFixslice.permbits_mod0)
+    Gen.qvalue_4x4x4
 
-let test_permbits_fixslice_mod_2 ienv functions =
-  let pb = symbol ienv functions "permbits_bitslice" in
-  let pf_mod0 = symbol ienv functions "permbits_fixslice_mod_0_4" in
-  let pf_mod1 = symbol ienv functions "permbits_fixslice_mod_1_" in
-  let pf_mod2 = symbol ienv functions "permbits_fixslice_mod_2_" in
-  let map_sigma = symbol ienv functions "map_rev_rotate_1" in
+let test_permbits_fixslice_mod_2 =
   test_eq_f ~verbose:Value.pp ~eq:Value.equal ~name:"Permbits fixslice : mod 2"
-    (pb $ pb $ pb)
-    (map_sigma $ map_sigma $ map_sigma $ pf_mod2 $ pf_mod1 $ pf_mod0)
-    qvalue_4x4x4
+    (GiftBitslice.permbits $ GiftBitslice.permbits $ GiftBitslice.permbits)
+    (GiftFixslice.map_rev_rotate_1 $ GiftFixslice.map_rev_rotate_1
+   $ GiftFixslice.map_rev_rotate_1 $ GiftFixslice.permbits_mod2
+   $ GiftFixslice.permbits_mod1 $ GiftFixslice.permbits_mod0)
+    Gen.qvalue_4x4x4
 
-let test_permbits_fixslice_mod_3 ienv functions =
-  let pb = symbol ienv functions "permbits_bitslice" in
-  let pf_mod0 = symbol ienv functions "permbits_fixslice_mod_0_4" in
-  let pf_mod1 = symbol ienv functions "permbits_fixslice_mod_1_" in
-  let pf_mod2 = symbol ienv functions "permbits_fixslice_mod_2_" in
-  let pf_mod3 = symbol ienv functions "permbits_fixslice_mod_3_" in
-  let map_sigma = symbol ienv functions "map_rev_rotate_1" in
+let test_permbits_fixslice_mod_3 =
   test_eq_f ~verbose:Value.pp ~eq:Value.equal ~name:"Permbits fixslice : mod 3"
-    (pb $ pb $ pb $ pb)
-    (map_sigma $ map_sigma $ map_sigma $ map_sigma $ pf_mod3 $ pf_mod2 $ pf_mod1
-   $ pf_mod0)
-    qvalue_4x4x4
+    (GiftBitslice.permbits $ GiftBitslice.permbits $ GiftBitslice.permbits
+   $ GiftBitslice.permbits)
+    (GiftFixslice.map_rev_rotate_1 $ GiftFixslice.map_rev_rotate_1
+   $ GiftFixslice.map_rev_rotate_1 $ GiftFixslice.map_rev_rotate_1
+   $ GiftFixslice.permbits_mod3 $ GiftFixslice.permbits_mod2
+   $ GiftFixslice.permbits_mod1 $ GiftFixslice.permbits_mod0)
+    Gen.qvalue_4x4x4
 
-let test_permbits_fixslice_mod_3_no_sigma ienv functions =
-  let pb = symbol ienv functions "permbits_bitslice" in
-  let pf_mod0 = symbol ienv functions "permbits_fixslice_mod_0_4" in
-  let pf_mod1 = symbol ienv functions "permbits_fixslice_mod_1_" in
-  let pf_mod2 = symbol ienv functions "permbits_fixslice_mod_2_" in
-  let pf_mod3 = symbol ienv functions "permbits_fixslice_mod_3_" in
+let test_permbits_fixslice_mod_3_no_sigma =
   test_eq_f ~verbose:Value.pp ~eq:Value.equal
     ~name:"Permbits fixslice : mod 3 (no sigma)"
-    (pb $ pb $ pb $ pb)
-    (pf_mod3 $ pf_mod2 $ pf_mod1 $ pf_mod0)
-    qvalue_4x4x4
+    (GiftBitslice.permbits $ GiftBitslice.permbits $ GiftBitslice.permbits
+   $ GiftBitslice.permbits)
+    (GiftFixslice.permbits_mod3 $ GiftFixslice.permbits_mod2
+   $ GiftFixslice.permbits_mod1 $ GiftFixslice.permbits_mod0)
+    Gen.qvalue_4x4x4
 
 let () =
   let open Alcotest in
   run "fixslice - test_reindex_fs.ua"
     [
-      ( "Transpose",
-        [ QCheck_alcotest.to_alcotest (test_compose_transposition env prog) ] );
+      ("Transpose", [ QCheck_alcotest.to_alcotest test_compose_transposition ]);
       ( "Sigma",
         [
-          QCheck_alcotest.to_alcotest (test_sigma_compose env prog);
-          QCheck_alcotest.to_alcotest (test_sigma4_id env prog);
+          QCheck_alcotest.to_alcotest test_sigma_compose;
+          QCheck_alcotest.to_alcotest test_sigma4_id;
         ] );
       ( "Permbits",
         [
-          QCheck_alcotest.to_alcotest
-            (test_permibits_fixslice_transformation env prog);
-          QCheck_alcotest.to_alcotest (test_permbits_fixslice_mod_0 env prog);
-          QCheck_alcotest.to_alcotest (test_permbits_fixslice_mod_1 env prog);
-          QCheck_alcotest.to_alcotest (test_permbits_fixslice_mod_2 env prog);
-          QCheck_alcotest.to_alcotest (test_permbits_fixslice_mod_3 env prog);
-          QCheck_alcotest.to_alcotest
-            (test_permbits_fixslice_mod_3_no_sigma env prog);
+          QCheck_alcotest.to_alcotest test_permibits_fixslice_transformation;
+          QCheck_alcotest.to_alcotest test_permbits_fixslice_mod_0;
+          QCheck_alcotest.to_alcotest test_permbits_fixslice_mod_1;
+          QCheck_alcotest.to_alcotest test_permbits_fixslice_mod_2;
+          QCheck_alcotest.to_alcotest test_permbits_fixslice_mod_3;
+          QCheck_alcotest.to_alcotest test_permbits_fixslice_mod_3_no_sigma;
         ] );
     ]
